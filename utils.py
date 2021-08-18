dbe23b0882d0f4b45a89d5fe5f88bdd58da26f76
mfmceneaney <mfm45@phy-grads05.local>
2021-08-18 16:45:53 -0400

    Fixed variable name typo

--- utils.py
+++ utils.py
@@ -836,7 +836,7 @@ def evaluate_on_data(model,device,dataset="ldata_6_22", prefix="", log_dir="logs
     argmax_Y = argmax_Y.cpu()
 
     # Get separated mass distributions
-    mass_Y    = ma.array(test_dataset.labels[:,0].clone().detach().float(),mask=~(argmax_Y == 1))
+    mass_sig_Y    = ma.array(test_dataset.labels[:,0].clone().detach().float(),mask=~(argmax_Y == 1))
     mass_bg_Y     = ma.array(test_dataset.labels[:,0].clone().detach().float(),mask=~(argmax_Y == 0))
 
     # Plot mass decisions separated into signal/background

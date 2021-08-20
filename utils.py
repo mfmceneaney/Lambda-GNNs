Fixed issue with scaling (again)

utils.py
@@ -298,57 +298,6 @@ def evaluate(model,device,dataset="ldata_6_22", prefix="", log_dir="logs/",verbo
     mass_sig_MC   = ma.array(test_dataset.labels[:,1].clone().detach().float(),mask=~(test_dataset.labels[:,0] == 1))
     mass_bg_MC    = ma.array(test_dataset.labels[:,1].clone().detach().float(),mask=~(test_dataset.labels[:,0] == 0))
 
-    # Plot mass decisions separated into signal/background
-    bins = 100
-    low_high = (1.1,1.13)
-    f = plt.figure()
-    plt.title(f'Separated mass distribution ep={epsilon:.2}')
-    (counts, hist) = np.histogram(mass_sig_Y[~mass_sig_Y.mask], bins=bins, range=low_high)
-    (counts2, hist2) = np.histogram(mass_bg_Y[~mass_bg_Y.mask], bins=bins, range=low_high)
-    plt.hist([hist[:-1],hist2[:-1]], color=['r','b'], label=['signal','background'], weights =[counts,counts2*epsilon*counts.sum()/counts2.sum()],
-             alpha=0.5, range=low_high, bins=bins, histtype='stepfilled', density=False)
-
-    # plt.hist(mass_sig_Y[~mass_sig_Y.mask], color='r', alpha=0.5, range=low_high, bins=bins, histtype='stepfilled', density=False, label='signal')
-    # plt.hist(mass_bg_Y[~mass_bg_Y.mask], color='b', alpha=0.5, range=low_high, bins=bins, histtype='stepfilled', density=False, label='background')
-    plt.legend(loc='upper left', frameon=False)
-    plt.ylabel('Counts')
-    plt.xlabel('Invariant mass (GeV)')
-    f.savefig(os.path.join(log_dir,'test_metrics_mass_'+datetime.datetime.now().strftime("%F")+dataset+'.png'))
-
-    # Plot correct mass decisions separated into signal/background
-    bins = 100
-    low_high = (1.1,1.13)
-    f = plt.figure()
-    plt.title(f'Separated mass distribution (true) ep={epsilon:.2f}')
-    (counts, hist) = np.histogram(mass_sig_true[~mass_sig_true.mask], bins=bins, range=low_high)
-    (counts2, hist2) = np.histogram(mass_bg_true[~mass_bg_true.mask], bins=bins, range=low_high)
-    plt.hist([hist[:-1],hist2[:-1]], color=['r','b'], label=['signal','background'], weights =[counts,counts2*epsilon*counts.sum()/counts2.sum()],
-             alpha=0.5, range=low_high, bins=bins, histtype='stepfilled', density=False)
-
-    # plt.hist(mass_sig_true[~mass_sig_true.mask], color='r', alpha=0.5, range=low_high, bins=bins, histtype='stepfilled', density=False, label='signal')
-    # plt.hist(mass_bg_true[~mass_bg_true.mask], color='b', alpha=0.5, range=low_high, bins=bins, histtype='stepfilled', density=False, label='background')
-    plt.legend(loc='upper left', frameon=False)
-    plt.ylabel('Counts')
-    plt.xlabel('Invariant mass (GeV)')
-    f.savefig(os.path.join(log_dir,'test_metrics_mass_true_'+datetime.datetime.now().strftime("%F")+dataset+'.png'))
-
-    # Plot incorrect mass decisions separated into signal/background
-    bins = 100
-    low_high = (1.1,1.13)
-    f = plt.figure()
-    plt.title(f'Separated mass distribution (false) ep={epsilon:.2f}')
-    (counts, hist) = np.histogram(mass_sig_false[~mass_sig_false.mask], bins=bins, range=low_high)
-    (counts2, hist2) = np.histogram(mass_bg_false[~mass_bg_false.mask], bins=bins, range=low_high)
-    plt.hist([hist[:-1],hist2[:-1]], color=['r','b'], label=['signal','background'], weights =[counts,counts2*epsilon*counts.sum()/counts2.sum()],
-             alpha=0.5, range=low_high, bins=bins, histtype='stepfilled', density=False)
-
-    # plt.hist(mass_sig_false[~mass_sig_false.mask], color='r', alpha=0.5, range=low_high, bins=bins, histtype='stepfilled', density=False, label='signal')
-    # plt.hist(mass_bg_false[~mass_bg_false.mask], color='b', alpha=0.5, range=low_high, bins=bins, histtype='stepfilled', density=False, label='background')
-    plt.legend(loc='upper left', frameon=False)
-    plt.ylabel('Counts')
-    plt.xlabel('Invariant mass (GeV)')
-    f.savefig(os.path.join(log_dir,'test_metrics_mass_false_'+datetime.datetime.now().strftime("%F")+dataset+'.png'))
-
     # Plot MC-Matched distributions
     bins = 100
     low_high = (1.1,1.13)
@@ -366,6 +315,60 @@ def evaluate(model,device,dataset="ldata_6_22", prefix="", log_dir="logs/",verbo
     plt.xlabel('Invariant mass (GeV)')
     f.savefig(os.path.join(log_dir,'mc_matched_mass_'+datetime.datetime.now().strftime("%F")+dataset+'.png'))
 
+    # Set correction factor for other hists using true MC ratio
+    factor = epsilon*counts.sum()/counts2.sum()
+
+    # Plot mass decisions separated into signal/background
+    bins = 100
+    low_high = (1.1,1.13)
+    f = plt.figure()
+    plt.title(f'Separated mass distribution ep={epsilon:.2}')
+    (counts, hist) = np.histogram(mass_sig_Y[~mass_sig_Y.mask], bins=bins, range=low_high)
+    (counts2, hist2) = np.histogram(mass_bg_Y[~mass_bg_Y.mask], bins=bins, range=low_high)
+    plt.hist([hist[:-1],hist2[:-1]], color=['r','b'], label=['signal','background'], weights =[counts,counts2*factor],
+             alpha=0.5, range=low_high, bins=bins, histtype='stepfilled', density=False)
+
+    # plt.hist(mass_sig_Y[~mass_sig_Y.mask], color='r', alpha=0.5, range=low_high, bins=bins, histtype='stepfilled', density=False, label='signal')
+    # plt.hist(mass_bg_Y[~mass_bg_Y.mask], color='b', alpha=0.5, range=low_high, bins=bins, histtype='stepfilled', density=False, label='background')
+    plt.legend(loc='upper left', frameon=False)
+    plt.ylabel('Counts')
+    plt.xlabel('Invariant mass (GeV)')
+    f.savefig(os.path.join(log_dir,'test_metrics_mass_'+datetime.datetime.now().strftime("%F")+dataset+'.png'))
+
+    # Plot correct mass decisions separated into signal/background
+    bins = 100
+    low_high = (1.1,1.13)
+    f = plt.figure()
+    plt.title(f'Separated mass distribution (true) ep={epsilon:.2f}')
+    (counts, hist) = np.histogram(mass_sig_true[~mass_sig_true.mask], bins=bins, range=low_high)
+    (counts2, hist2) = np.histogram(mass_bg_true[~mass_bg_true.mask], bins=bins, range=low_high)
+    plt.hist([hist[:-1],hist2[:-1]], color=['r','b'], label=['signal','background'], weights =[counts,counts2*factor],
+             alpha=0.5, range=low_high, bins=bins, histtype='stepfilled', density=False)
+
+    # plt.hist(mass_sig_true[~mass_sig_true.mask], color='r', alpha=0.5, range=low_high, bins=bins, histtype='stepfilled', density=False, label='signal')
+    # plt.hist(mass_bg_true[~mass_bg_true.mask], color='b', alpha=0.5, range=low_high, bins=bins, histtype='stepfilled', density=False, label='background')
+    plt.legend(loc='upper left', frameon=False)
+    plt.ylabel('Counts')
+    plt.xlabel('Invariant mass (GeV)')
+    f.savefig(os.path.join(log_dir,'test_metrics_mass_true_'+datetime.datetime.now().strftime("%F")+dataset+'.png'))
+
+    # Plot incorrect mass decisions separated into signal/background
+    bins = 100
+    low_high = (1.1,1.13)
+    f = plt.figure()
+    plt.title(f'Separated mass distribution (false) ep={epsilon:.2f}')
+    (counts, hist) = np.histogram(mass_sig_false[~mass_sig_false.mask], bins=bins, range=low_high)
+    (counts2, hist2) = np.histogram(mass_bg_false[~mass_bg_false.mask], bins=bins, range=low_high)
+    plt.hist([hist[:-1],hist2[:-1]], color=['r','b'], label=['signal','background'], weights =[counts,counts2*factor],
+             alpha=0.5, range=low_high, bins=bins, histtype='stepfilled', density=False)
+
+    # plt.hist(mass_sig_false[~mass_sig_false.mask], color='r', alpha=0.5, range=low_high, bins=bins, histtype='stepfilled', density=False, label='signal')
+    # plt.hist(mass_bg_false[~mass_bg_false.mask], color='b', alpha=0.5, range=low_high, bins=bins, histtype='stepfilled', density=False, label='background')
+    plt.legend(loc='upper left', frameon=False)
+    plt.ylabel('Counts')
+    plt.xlabel('Invariant mass (GeV)')
+    f.savefig(os.path.join(log_dir,'test_metrics_mass_false_'+datetime.datetime.now().strftime("%F")+dataset+'.png'))
+
     # Plot MC-Matched distributions for NN-identified signal
     bins = 100
     low_high = (1.1,1.13)
@@ -373,7 +376,7 @@ def evaluate(model,device,dataset="ldata_6_22", prefix="", log_dir="logs/",verbo
     plt.title(f'NN-identified signal mass distribution MC-matched ep={epsilon:.2f}')
     (counts, hist) = np.histogram(mass_sig_true[~mass_sig_true.mask], bins=bins, range=low_high)
     (counts2, hist2) = np.histogram(mass_sig_false[~mass_sig_false.mask], bins=bins, range=low_high)
-    plt.hist([hist[:-1],hist2[:-1]], color=['m','c'], label=['true','false'], weights =[counts,counts2*epsilon*counts.sum()/counts2.sum()],
+    plt.hist([hist[:-1],hist2[:-1]], color=['m','c'], label=['true','false'], weights =[counts,counts2*factor],
              alpha=0.5, range=low_high, bins=bins, histtype='stepfilled', density=False)
 
     # plt.hist(mass_sig_true[~mass_sig_true.mask], color='m', alpha=0.5, range=low_high, bins=bins, histtype='stepfilled', density=False, label='true')
@@ -390,7 +393,7 @@ def evaluate(model,device,dataset="ldata_6_22", prefix="", log_dir="logs/",verbo
     plt.title(f'NN-identified bg mass distribution MC-matched ep={epsilon:.2f}')
     (counts, hist) = np.histogram(mass_bg_true[~mass_bg_true.mask], bins=bins, range=low_high)
     (counts2, hist2) = np.histogram(mass_bg_false[~mass_bg_false.mask], bins=bins, range=low_high)
-    plt.hist([hist[:-1],hist2[:-1]], color=['m','c'], label=['true','false'], weights =[counts,counts2*epsilon*counts.sum()/counts2.sum()],
+    plt.hist([hist[:-1],hist2[:-1]], color=['m','c'], label=['true','false'], weights =[counts,counts2*factor],
              alpha=0.5, range=low_high, bins=bins, histtype='stepfilled', density=False)
 
     # plt.hist(mass_bg_true[~mass_bg_true.mask], color='m', alpha=0.5, range=low_high, bins=bins, histtype='stepfilled', density=False, label='true')
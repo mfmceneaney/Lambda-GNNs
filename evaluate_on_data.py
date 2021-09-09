f0f5b6f14f01fca02bf196b5959c997921039734
mfmceneaney <mfm45@phy-grads05.local>
2021-09-09 16:48:26 -0400

    Fixed typo in `evaluate_on_data.py`

--- evaluate_on_data.py
+++ evaluate_on_data.py
@@ -102,7 +102,7 @@ def main():
     except FileExistsError: print('Directory:',args.log,'already exists!')
 
     # Train model
-    evaluate_on_data(model, device, dataset=args.dataset, prefix=args.prefix, split=args.split log_dir=args.log, verbose=args.verbose)
+    evaluate_on_data(model, device, dataset=args.dataset, prefix=args.prefix, split=args.split, log_dir=args.log, verbose=args.verbose)
     if args.verbose: plt.show()
 
 if __name__ == '__main__':

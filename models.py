9d02bda300ade8b3c1995deb7effed266cc9be4d
Matthew McEneaney <matthew.mceneaney@duke.edu>
2022-07-22 12:05:14 -0400

    Added new MLP class with sigmoid activation.

--- models.py
+++ models.py
@@ -112,6 +112,59 @@ class ApplyNodeFunc(nn.Module):
         h = F.relu(h)
         return h
 
+class MLP_SIGMOID(nn.Module):
+    """MLP_SIGMOID with linear output"""
+    def __init__(self, num_layers, input_dim, hidden_dim, output_dim):
+        """MLP_SIGMOID layers construction
+
+        Parameters
+        ----------
+        num_layers: int
+            The number of linear layers
+        input_dim: int
+            The dimensionality of input features
+        hidden_dim: int
+            The dimensionality of hidden units at ALL layers
+        output_dim: int
+            The number of classes for prediction
+
+        """
+        super(MLP_SIGMOID, self).__init__()
+        self.linear_or_not = True  # default is linear model
+        self.num_layers = num_layers
+        self.output_dim = output_dim
+
+        if num_layers < 1:
+            raise ValueError("number of layers should be positive!")
+        elif num_layers == 1:
+            # Linear model
+            self.linear = nn.Linear(input_dim, output_dim)
+        else:
+            # Multi-layer model
+            self.linear_or_not = False
+            self.linears = torch.nn.ModuleList()
+            self.batch_norms = torch.nn.ModuleList()
+
+            self.linears.append(nn.Linear(input_dim, hidden_dim))
+            for layer in range(num_layers - 2):
+                self.linears.append(nn.Linear(hidden_dim, hidden_dim))
+            self.linears.append(nn.Linear(hidden_dim, output_dim))
+
+            for layer in range(num_layers - 1):
+                self.batch_norms.append(nn.BatchNorm1d((hidden_dim)))
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        if self.linear_or_not:
+            # If linear model
+            return self.sigmoid(self.linear(x))
+        else:
+            # If MLP
+            h = x
+            for i in range(self.num_layers - 1):
+                h = F.relu(self.batch_norms[i](self.linears[i](h)))
+            return self.sigmoid(self.linears[-1](h))
+
 
 class MLP(nn.Module):
     """MLP with linear output"""
